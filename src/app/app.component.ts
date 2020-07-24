8e729a047dfaa363e671b2e64b7ac950c453bda0
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-24 02:59:50 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vrarar';
+  title = 'world weather map';
+  latitude = 51.678418;
+  longitude = 7.809007;
+
+  onChoseLocation(event){
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+
+  } 
 }
